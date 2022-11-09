3b24f8834dddf2a2f10535a67719226d14411778
Carolina Carranza <00008119@uca.edu.sv>
2022-11-08 22:10:34 -0600

    :lipstick: change primary button style

--- Client/n-pupas/components/buttons/primary.jsx
+++ Client/n-pupas/components/buttons/primary.jsx
@@ -2,7 +2,7 @@ const PrimaryButton = ({ text }) => {
   return (
     <button
       type='submit'
-      className='w-full px-6 py-2 bg-primary-500 font-bold text-white uppercase rounded-md border-2 border-transparent cursor-pointer hover:bg-primary-700 transform transition duration-300 ease-in-out'
+      className='px-10 py-3 bg-primary-500 font-medium tracking-wider text-white uppercase rounded-md border-2 border-transparent cursor-pointer hover:bg-primary-700 transform transition duration-300 ease-in-out hover:opacity-80'
     >
       {text}
     </button>
