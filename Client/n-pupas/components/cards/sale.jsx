350716e08c03e693935d2519326e806725b1b7f6
Carolina Carranza <00008119@uca.edu.sv>
2022-11-13 01:47:13 -0600

    :lipstick: add new styles to sales admin page

--- Client/n-pupas/components/cards/sale.jsx
+++ Client/n-pupas/components/cards/sale.jsx
@@ -3,8 +3,11 @@ import { useRouter } from 'next/router';
 import { adminRoutes } from 'routes/routes';
 import ActionButton from 'components/buttons/actionButton';
 import { actionButtons } from 'constants/data';
+import useToggle from 'hooks/useToggle';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
 
 const SaleCard = ({ sale, total, onDeleteHandler }) => {
+  const [isExpanded, toggleIsExpanded] = useToggle();
   const router = useRouter();
 
   const handleOnUpdate = () => {
@@ -16,40 +19,51 @@ const SaleCard = ({ sale, total, onDeleteHandler }) => {
   };
 
   return (
-    <article className='bg-white shadow-md p-4'>
-      <div className='flex flex-col mb-2  '>
-        <h1 className='text-gray-500 font-bold text-xl'>Venta #{sale.id}</h1>
-        <p>{` ${sale.saleDate}`}</p>
-        {sale.details.length > 0 &&
-          sale.details.map(detail => {
-            return (
-              <div
-                key={detail.id}
-                className='font-medium flex flex-auto justify-between items-start mt-4 border-l-2 border-primary-500 border-opacity-50 pl-2'
-              >
-                <div>
-                  <p>
-                    {detail.product.name} x{detail.amount}
-                  </p>
-                  {detail.massDetails && detail.massDetails.mass && (
-                    <p className='text-gray-500 text-sm'>{detail.massDetails.mass.mass}</p>
-                  )}
-                </div>
-
-                <p>{`$${detail.total.toFixed(2)}`}</p>
-              </div>
-            );
-          })}
-
-        <div className='font-bold flex flex-auto justify-between items-center text-primary-500 mt-2'>
-          <p>Total</p>
-          <p>${total.toFixed(2)}</p>
+    <article className='bg-white shadow-md cursor-pointer'>
+      <div
+        onClick={() => toggleIsExpanded(!isExpanded)}
+        className={`${
+          isExpanded ? 'bg-primary-300 bg-opacity-10' : 'bg-white'
+        } shadow-md p-4 flex flex-col gap-1.5`}
+      >
+        <div className='flex justify-between'>
+          <h1 className='text-primary-500 font-semibold '>Venta #{sale.id}</h1>
+          <p className='text-primary-500 font-semibold '>${total.toFixed(2)}</p>
+          {isExpanded ? <ChevronUpIcon className='w-6' /> : <ChevronDownIcon className='w-6' />}
         </div>
-        <div className='flex gap-2'>
-          <ActionButton actionElements={actionButtons.edit} onClickHandler={handleOnUpdate} />
-          <ActionButton actionElements={actionButtons.delete} onClickHandler={handleOnDelete} />
-        </div>
+        <p className='font-light text-sm'>{` ${sale.saleDate}`}</p>
       </div>
+      {isExpanded && (
+        <div className='p-4 pt-3'>
+          {sale.details.length > 0 ? (
+            sale.details.map(detail => {
+              return (
+                <div
+                  key={detail.id}
+                  className='font-medium flex flex-auto justify-between items-start mt-4 border-l-2 border-primary-500 border-opacity-50 pl-2'
+                >
+                  <div>
+                    <p>
+                      {detail.product.name} x{detail.amount}
+                    </p>
+                    {detail.massDetails && detail.massDetails.mass && (
+                      <p className='text-gray-500 text-sm'>{detail.massDetails.mass.mass}</p>
+                    )}
+                  </div>
+
+                  <p>{`$${detail.total.toFixed(2)}`}</p>
+                </div>
+              );
+            })
+          ) : (
+            <></>
+          )}
+          <div className='flex gap-5 mt-5 justify-center'>
+            <ActionButton actionElements={actionButtons.edit} onClickHandler={handleOnUpdate} />
+            <ActionButton actionElements={actionButtons.delete} onClickHandler={handleOnDelete} />
+          </div>
+        </div>
+      )}
     </article>
   );
 };
