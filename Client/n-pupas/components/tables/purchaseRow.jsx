00ac933368ea5e048d0d9aff2a57900a5e0ac74e
Carolina Carranza <00008119@uca.edu.sv>
2022-11-13 01:46:58 -0600

    :lipstick: update styles of components

--- Client/n-pupas/components/tables/purchaseRow.jsx
+++ Client/n-pupas/components/tables/purchaseRow.jsx
@@ -1,8 +1,8 @@
 import { useRouter } from 'next/router';
 
-import { adminRoutes } from 'routes/routes';
 import ActionButton from 'components/buttons/actionButton';
 import { actionButtons } from 'constants/data';
+import { adminRoutes } from 'routes/routes';
 
 const PurchaseTableRow = ({ purchase, extended = false, onDeleteHandler = null }) => {
   const router = useRouter();
@@ -19,15 +19,15 @@ const PurchaseTableRow = ({ purchase, extended = false, onDeleteHandler = null }
 
   return (
     <tr className='bg-white border-[1px] border-primary-300'>
-      <td className='text-center px-3 py-2 font-bold whitespace-nowrap border-r-[1px] border-primary-300 truncate'>
+      <td className='text-center px-3 py-2 whitespace-nowrap border-r-[1px] border-primary-300 truncate'>
         {purchase.concept}
       </td>
-      <td className='px-3 py-2  font-bold border-primary-300 border-r-[1px] whitespace-nowrap'>
+      <td className='px-3 py-2 border-primary-300 border-r-[1px] whitespace-nowrap'>
         ${purchase.amount.toFixed(2)}
       </td>
       {extended && (
         <>
-          <td className='text-center px-3 py-2 font-bold whitespace-nowrap border-r-[1px] border-primary-300 truncate'>
+          <td className='text-center px-3 py-2 whitespace-nowrap border-r-[1px] border-primary-300 truncate'>
             {purchase.purchaseDate}
           </td>
           <td className='flex justify-center items-center px-3 py-2 gap-2 whitespace-nowrap'>
