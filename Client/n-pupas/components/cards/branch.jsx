829fdb671271ec83b447034c20dd43b07942a5e8
NNieto25 <00199019@uca.edu.sv>
2022-11-19 18:24:02 -0600

    Fixed designs for branches section

--- Client/n-pupas/components/cards/branch.jsx
+++ Client/n-pupas/components/cards/branch.jsx
@@ -16,12 +16,16 @@ const BranchCard = ({ branch, onDeleteHandler }) => {
   };
 
   return (
-    <article className='bg-white shadow-md p-4'>
+    <article className='bg-white shadow-md w-96'>
       <div className='flex flex-col mb-2'>
-        <h2 className='font-bold'>{branch.name}</h2>
-        <p> {branch.address} </p>
-        <p> {`Desde: ${branch.openingDate}`} </p>
-        <div className='flex gap-2'>
+        <h2 className='font-bold text-3xl bg-primary-500 text-white py-5 pl-4'>{branch.name}</h2>
+        <div className='grid grid-cols-5 gap-3'>
+          <p className='ml-4 my-4 w-4 col-span-1 text-primary-300'> Dirección </p>
+          <p className='ml-12 mt-4 col-span-4'> {branch.address} </p>
+          <p className='ml-4 my-2 col-span-1 text-primary-300'> Apertura </p>
+          <p className='ml-12 my-2 col-span-4'> {branch.openingDate} </p>
+        </div>
+        <div className='flex gap-2 mt-12 justify-end mr-4 mb-1/2'>
           <ActionButton actionElements={actionButtons.edit} onClickHandler={handleOnModify} />
           <ActionButton actionElements={actionButtons.delete} onClickHandler={handleOnDelete} />
         </div>
--- Client/n-pupas/components/information/page-heading.jsx
+++ Client/n-pupas/components/information/page-heading.jsx
@@ -3,7 +3,7 @@ import AddButton from 'components/buttons/add';
 const PageHeading = ({ title, route, text }) => {
   return (
     <div className='flex flex-wrap gap-3 justify-between mb-2 items-center'>
-      <h1 className='font-bold text-2xl sm:text-3xl'>{title}</h1>
+      <h1 className='font-bold text-2xl sm:text-3xl text-primary-500'>{title}</h1>
       <AddButton route={route} text={text} />
     </div>
   );
