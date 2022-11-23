f8dc0954ac8ab01e0066a29f3d8a816fcb10d5a4
MoralesWJosue98 <wilfredo.josue@live.com>
2022-11-22 18:13:12 -0600

    Fix: Responsive Behavior and Add: Commented fixes

--- Client/n-pupas/pages/empleado/informe.jsx
+++ Client/n-pupas/pages/empleado/informe.jsx
@@ -19,13 +19,8 @@ const ReportPage = ({ employee }) => {
   const monthlyRent = montlyDiscounts.rent(employee.salary).toFixed(2);
   const biweeklyRent = biweeklyDiscounts.rent(employee.salary).toFixed(2);
 
-  return (
+  return (/*
     <main className='p-6 flex flex-col gap-5'>
-      <Head>
-        <title>{employeePages.report}</title>
-      </Head> 
-
-      <h1 className='font-bold text-3xl md:text-left text-primary-500 '>{employeePages.report}</h1>
 
       <div className='grid grid-cols-2 gap-6 flex-wrap relative overflow-x-auto'>
         <section className='bg-secondary-500 p-5 bg-opacity-40 rounded-lg shadow-md '>
@@ -119,6 +114,109 @@ const ReportPage = ({ employee }) => {
           </div>
         </section>
       </div>
+    </main>*/
+    <main className='p-6 flex flex-col gap-5'>
+      <Head>
+        <title>{employeePages.report}</title>
+      </Head> 
+
+      <h1 className='font-bold text-3xl text-center md:text-left text-primary-500 '>{employeePages.report}</h1>
+
+      <div className='flex flex-col md:flex-row p-5 gap-y-10 gap-x-8 '>
+        <section className='bg-secondary-500 p-5 bg-opacity-40 rounded-lg shadow-md w-fit md:basis-2/4'>
+          <div className='grid justify-items-start mb-5 '>
+              <h2 className='text-xl md:text-2xl font-bold text-primary-500 mb-5'>{titles.information} </h2>
+              <div className='grid grid-rows-3 grid-flow-col'>
+                <div className='mb-5 p-3 border-l-4 border-violet-500'>
+                  <p className=' text-primary-400 pl-4'>NOMBRE</p>
+                  <p className='text-base pl-4'>{employee.user.name}</p>
+                </div>
+                <div className='mb-5 p-3 border-l-4 border-violet-500'>
+                  <p className='text-primary-400 pl-4 '>EMPLEADO DESDE</p>
+                  <p className='text-base pl-4'>{employee.hiringDate}</p>
+                </div>
+                <div className='mb-5 p-3 border-l-4 border-violet-500'>
+                  <p className='text-primary-400 pl-4'>TIEMPO LABORADO</p>
+                  <p className='text-base pl-4'>{getPeriodOfTime(employee.hiringDate)}</p>
+                </div>
+              </div>
+          </div>
+        </section>
+        <section className='bg-secondary-500 p-5 bg-opacity-40 rounded-lg shadow-md w-fit md:w-full'>
+        <div className='flex grid mb-5 justify-items-start'>
+            <h2 className='text-xl md:text-2xl font-bold text-primary-500 mb-5'>{titles.salary} </h2>
+            <p className='text-xl text-primary-500 pl-2'>Salario mensual: 
+                <span className='pl-3 md:inline-flex md:items-baseline pl-5'>
+                  <span className='text-xl font-bold text-primary-500'>
+                    ${employee.salary}
+                  </span>
+                </span>
+            </p> 
+            <table className='md:w-full text-sm text-left mt-5 md:p-8 border-[1px] border-violet-400'>
+              <thead className='bg-[#584076] bg-auto text-base  text-white text-center border-[1px] border-violet-400'>
+                <th>Descuento</th>
+                <th>Mensual</th>
+                <th>Quincenal</th>
+              </thead>
+              <tbody className='bg-white '>
+                <tr className=''>
+                  <th
+                    scope='row'
+                    className='text-center font-medium whitespace-nowrap p-3 border-[1px] border-violet-300'
+                  >
+                    ISSS
+                  </th>
+                  <td className='text-center font-medium border-[1px] border-violet-300'>${monthlyISSS}</td>
+                  <td className='text-center font-medium border-[1px] border-violet-300'>${biweeklyISSS}</td> 
+                </tr>
+
+                <tr className='bg-white '>
+                  <th
+                    scope='row'
+                    className='text-center font-medium whitespace-nowrap  p-3 border-[1px] border-violet-300'
+                  >
+                    AFP
+                  </th>
+                  <td className='text-center font-medium border-[1px] border-violet-300'>${monthlyAFP}</td>
+                  <td className='text-center font-medium border-[1px] border-violet-300'>${biweeklyAFP}</td>
+                </tr>
+                <tr className='bg-white '>
+                  <th
+                    scope='row'
+                    className='text-center font-medium whitespace-nowrap  p-3 border-[1px] border-violet-300'
+                  >
+                    ISR
+                  </th>
+                  <td className='text-center font-medium border-[1px] border-violet-300'>${monthlyRent}</td>
+                  <td className='text-center font-medium border-[1px] border-violet-300'>${biweeklyRent}</td>
+                </tr>
+              </tbody>
+          </table>
+          </div>
+          <div className='flex flex-col md:flex-row md:justify-between gap-1 mt-6'>
+            <div className='bg-[#8E6FB6] bg-opacity-30 rounded-[30px] shadow-md h-10 w-64 mb-4'>
+              <p className='mt-2 ml-2 text-primary-500'>Salario neto mensual: 
+                  <span className=''>
+                    <span className='font-bold text-primary-500 pl-1'>
+                    ${employee.salary - monthlyISSS - monthlyAFP - monthlyRent}
+                    </span>
+                  </span>
+              </p> 
+            </div>
+            <section className='bg-[#8E6FB6] bg-opacity-30 rounded-[30px] shadow-md h-10 w-64 '>
+              <p className='mt-2 ml-2 text-primary-500'>Salario neto quincenal: 
+                    <span className=''>
+                      <span className='font-bold text-primary-500 pl-1'>
+                      ${employee.salary / 2 - biweeklyISSS - biweeklyAFP - biweeklyRent}
+                      </span>
+                    </span>
+                </p> 
+            </section>
+          </div>
+          
+        </section>
+      </div>
+
     </main>
   );
 };
