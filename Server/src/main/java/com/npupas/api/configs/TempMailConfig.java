2d7e610ca780fc94d19245bf738c20c9815737ae
Nelson Navarro <38149702+NelNav1222@users.noreply.github.com>
2023-06-05 19:22:11 -0600

    set: mail configuration template

---
+++ Server/src/main/java/com/npupas/api/configs/TempMailConfig.java
@@ -0,0 +1,38 @@
+package com.npupas.api.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import java.nio.charset.StandardCharsets;
+
+@Configuration
+public class TempMailConfig {
+
+    @Value("${thymeleaf.prefix}")
+    private String prefix;
+
+    @Value("${thymeleaf.suffix}")
+    private String suffix;
+
+    @Bean
+    public SpringTemplateEngine springTemplateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addTemplateResolver(htmlTemplateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver htmlTemplateResolver(){
+        SpringResourceTemplateResolver emailTemplateResolver = new SpringResourceTemplateResolver();
+        emailTemplateResolver.setPrefix(prefix);
+        emailTemplateResolver.setSuffix(suffix);
+        emailTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        emailTemplateResolver.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        return emailTemplateResolver;
+    }
+
+}
