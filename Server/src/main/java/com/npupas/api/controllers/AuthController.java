faf060d0e2afdedce6eac25cf2724326f75f9dbf
Nelson Navarro <38149702+NelNav1222@users.noreply.github.com>
2023-06-06 07:48:53 -0600

    modified: controller for sending email register

--- Server/src/main/java/com/npupas/api/controllers/AuthController.java
+++ Server/src/main/java/com/npupas/api/controllers/AuthController.java
@@ -2,7 +2,10 @@
 
 import javax.validation.Valid;
 
+import com.npupas.api.models.dtos.*;
+import com.npupas.api.services.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -11,37 +14,50 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.npupas.api.models.dtos.LoginDTO;
-import com.npupas.api.models.dtos.MessageDTO;
-import com.npupas.api.models.dtos.RegistrationFormDTO;
-import com.npupas.api.models.dtos.TokenDTO;
 import com.npupas.api.models.entities.User;
 import com.npupas.api.services.UserService;
 import com.npupas.api.utils.TokenManager;
 
-@CrossOrigin
+import java.util.HashMap;
+import java.util.Map;
+
+	@CrossOrigin
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+
+	@Value("${mail.email}")
+	private String email;
+
+	@Value("${mail.subject}")
+	private String subject;
+
+	@Value("${mail.sign}")
+	private String sign;
+
+	@Value("${mail.location}")
+	private String location;
 
 	@Autowired
 	UserService userService;
 	@Autowired
 	TokenManager tokenManager;
-	
+	@Autowired
+	private EmailService emailService;
+
 	@PostMapping("/login")
 	private ResponseEntity<TokenDTO> login(@Valid LoginDTO loginInfo, BindingResult result) {
 	    try {
-	        
+
 	        if(result.hasErrors()) {
 	            return new ResponseEntity<>(
 	                    new TokenDTO(),
 	                    HttpStatus.BAD_REQUEST
 	                );
 	        }
-	        
+
 	        User user = userService.getUserByUsername(loginInfo.getUsername());
-	        
+
 	        if(!userService.comparePassword(user, loginInfo.getPassword())) {
 	            return new ResponseEntity<>(
 	                    new TokenDTO(),
@@ -84,8 +100,19 @@ public class AuthController {
 			}
 
 			userService.register(formDTO);
+			MailMessageDTO mailMessage = new MailMessageDTO();
+			mailMessage.setMailTo(formDTO.getUsername());
+			mailMessage.setSubject(subject);
+			mailMessage.setFrom(email);
+			Map<String, Object> model = new HashMap<String, Object>();
+			model.put("name", formDTO.getName());
+			model.put("location", location);
+			model.put("sign", sign);
+			mailMessage.setProps(model);
+			emailService.sendWelcomeEmail(mailMessage);
 
 			return new ResponseEntity<>(new MessageDTO("Usuario registrado exitosamente"), HttpStatus.CREATED);
+
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			return new ResponseEntity<>(new MessageDTO("Error interno del servidor"), HttpStatus.INTERNAL_SERVER_ERROR);
