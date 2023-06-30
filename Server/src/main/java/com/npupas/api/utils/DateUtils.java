696c1c9a8d7083726dc28be3bfca0187c202b561
NNieto25 <00199019@uca.edu.sv>
2023-06-29 20:54:20 -0600

    Added function for date instant

--- Server/src/main/java/com/npupas/api/utils/DateUtils.java
+++ Server/src/main/java/com/npupas/api/utils/DateUtils.java
@@ -3,6 +3,7 @@ package com.npupas.api.utils;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtils {
@@ -13,5 +14,17 @@ public class DateUtils {
         } catch (ParseException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static Long getDateInstant(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        return calendar.getTimeInMillis();
     }
 }
