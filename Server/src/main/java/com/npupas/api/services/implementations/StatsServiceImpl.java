dc5eacac15d453a6c02dd76814cfbf0564925972
NNieto25 <00199019@uca.edu.sv>
2023-07-02 00:50:47 -0600

    Service implementation for linechart data

--- Server/src/main/java/com/npupas/api/services/implementations/StatsServiceImpl.java
+++ Server/src/main/java/com/npupas/api/services/implementations/StatsServiceImpl.java
@@ -2,14 +2,17 @@ package com.npupas.api.services.implementations;
 
 import com.npupas.api.models.dtos.*;
 import com.npupas.api.models.entities.Admin;
+import com.npupas.api.projections.LineChartStat;
 import com.npupas.api.repositories.StatsRepository;
 import com.npupas.api.services.AdminService;
 import com.npupas.api.services.StatsService;
+import com.npupas.api.utils.DateUtils;
 import com.npupas.api.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 import static com.npupas.api.utils.DateUtils.getDateInstant;
 
@@ -78,6 +81,37 @@ public class StatsServiceImpl implements StatsService {
         Admin admin = adminService.getAdminByToken(Utils.getToken(entireToken));
 
         return repository.getBranchesMonthCategorySalesStats(admin.getID()).stream().map(PairStatDTO::new).toList();
+    }
+
+    @Override
+    public LineChartGroupDTO getLinechartMonthStats(Long branchId) {
+        try {
+            Date now = new Date();
+
+            CompletableFuture<LineChartStat> thisMonthSales = CompletableFuture.supplyAsync(() -> repository.getThisMonthSalesLinechartStats(branchId));
+            CompletableFuture<LineChartStat> lastMonthSales = CompletableFuture.supplyAsync(() -> repository.getLastMonthSalesLinechartStats(branchId));
+            CompletableFuture<LineChartStat> thisMonthPurchases = CompletableFuture.supplyAsync(() -> repository.getThisMonthPurchasesLinechartStats(branchId));
+            CompletableFuture<LineChartStat> lastMonthPurchases = CompletableFuture.supplyAsync(() -> repository.getLastMonthPurchasesLinechartStats(branchId));
+
+            CompletableFuture<Void> allStats = CompletableFuture.allOf(thisMonthSales, lastMonthSales, thisMonthPurchases, lastMonthPurchases);
+            allStats.get();
+
+            LineChartStatDTO pastSales = new LineChartStatDTO(lastMonthSales.get());
+            LineChartStatDTO presentSales = new LineChartStatDTO(thisMonthSales.get());
+            LineChartStatDTO pastPurchases = new LineChartStatDTO(lastMonthPurchases.get());
+            LineChartStatDTO presentPurchases = new LineChartStatDTO(thisMonthPurchases.get());
+
+            LineChartStatDTO pastRevenue = new LineChartStatDTO(Optional.ofNullable(pastSales.getTotalSales()).orElse(0.0) - Optional.ofNullable(pastPurchases.getTotalSales()).orElse(0.0), 0);
+            LineChartStatDTO presentRevenue = new LineChartStatDTO(Optional.ofNullable(presentSales.getTotalSales()).orElse(0.0) - Optional.ofNullable(presentPurchases.getTotalSales()).orElse(0.0), 0);
+
+            LineChartComparisonDTO sales = new LineChartComparisonDTO(pastSales, presentSales);
+            LineChartComparisonDTO purchases = new LineChartComparisonDTO(pastPurchases, presentPurchases);
+            LineChartComparisonDTO revenue = new LineChartComparisonDTO(pastRevenue, presentRevenue);
+
+            return new LineChartGroupDTO(sales, purchases, revenue);
+        } catch (Exception e){
+            return null;
+        }
     }
 
     private List<RangeStatDTO> buildRangeStats(List<SaleStatDTO> stats) {
