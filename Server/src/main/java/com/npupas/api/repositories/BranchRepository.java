458a4251a4ffab65b43a9c4a85243cd3a06a17d7
Carolina Carranza <00008119@uca.edu.sv>
2023-07-05 11:12:07 -0600

    Add logic for branches service to include opening and closing hour

--- Server/src/main/java/com/npupas/api/repositories/BranchRepository.java
+++ Server/src/main/java/com/npupas/api/repositories/BranchRepository.java
@@ -2,12 +2,19 @@ package com.npupas.api.repositories;
 
 import java.util.List;
 
+import com.npupas.api.models.entities.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.npupas.api.models.entities.Branch;
 import com.npupas.api.models.entities.Pupuseria;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BranchRepository extends JpaRepository<Branch, Long> {
 	List<Branch> findByPupuseria(Pupuseria pupuseria);
-	
+
+	List<Branch> getByPupuseriaAdminIsNot(Admin admin);
+
+	@Query("SELECT DISTINCT b FROM branch b WHERE b.id = :id")
+	List<Branch> findBranchById(@Param("id") Long id);
 }
